e675790d78edeac01dc03f8e44dbaaedde52d740
Ian Alves <ian.santos.alves@hotmail.com>
2023-05-20 14:42:53 -0300

    Configurando variaveis de ambiente com a lib dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.port;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
