64924d3bf638d2abb073250bf9e9f651142d32db
Ian Alves <ian.santos.alves@hotmail.com>
2023-05-28 20:47:48 -0300

    criado método getAllUsers para retornar todos os users do banco

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,4 +17,13 @@ module.exports = {
             return res.status(500).json({ error: error });
         }
     },
+
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await User.find();
+            return res.status(200).json(users);
+        } catch (error) {
+            return res.status(500).json({ error: error });
+        }
+    },
 };
