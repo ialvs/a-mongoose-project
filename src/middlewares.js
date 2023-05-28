ed41ec7a7119d5caefe3ccc34456d3f7cdf2a32c
Ian Alves <ian.santos.alves@hotmail.com>
2023-05-28 20:11:43 -0300

    consertando middleware de validação de usuário

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,21 +3,25 @@ module.exports = {
         const { name, password, course, active } = req.body;
 
         if (!name) {
-            res.status(400).json({ messagem: "User name must be informed" });
+            return res
+                .status(400)
+                .json({ messagem: "User name must be informed" });
         }
 
         if (!password) {
-            res.status(400).json({
+            return res.status(400).json({
                 messagem: "User password must be informed",
             });
         }
 
         if (!course) {
-            res.status(400).json({ messagem: "User course must be informed" });
+            return res
+                .status(400)
+                .json({ messagem: "User course must be informed" });
         }
 
         if (typeof active !== "boolean") {
-            res.status(400).json({
+            return res.status(400).json({
                 messagem: "User availability must be informed (as true/false)",
             });
         }
