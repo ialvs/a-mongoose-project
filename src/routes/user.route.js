56ee03e696dcb4fd6831687cad83100b328d3502
Ian Alves <ian.santos.alves@hotmail.com>
2023-05-28 21:00:42 -0300

    adicionado controlador para get de user a partir de id

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { createAnUser, getAllUsers } = require("../controllers/user.controller");
+const {
+    createAnUser,
+    getAllUsers,
+    getUserById,
+} = require("../controllers/user.controller");
 const { validatePostedUser } = require("../middlewares/user.middleware");
 
 const routes = express.Router();
 
 routes.get("/", getAllUsers);
+routes.get("/:id", getUserById);
 routes.post("/", validatePostedUser, createAnUser);
 
 module.exports = routes;
