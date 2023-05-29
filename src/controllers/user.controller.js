7e001929553c965bad0df4705f491b667acda23a
Ian Alves <ian.santos.alves@hotmail.com>
2023-05-28 21:00:11 -0300

    adicionado controller getUserById para retornar um usuário a partir de seu id

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,4 +26,19 @@ module.exports = {
             return res.status(500).json({ error: error });
         }
     },
+
+    getUserById: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const user = await User.findById(id);
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: "Usuário não encontrado." });
+            }
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({ error: error });
+        }
+    },
 };
